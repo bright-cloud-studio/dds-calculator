74bab61bca22085a5083039787da460b4ddb32c4
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-18 12:59:37 -0400

    Create tl_calculator_submission.php

---
+++ src/Resources/contao/languages/en/tl_calculator_submission.php
@@ -0,0 +1,29 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_calculator_submission']['new']                        = array('New Alert Email', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['show']                       = array('Alert Email Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['edit']                       = array('Edit Alert Email', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['copy']                       = array('Copy Alert Email', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['delete']                     = array('Delete Alert Email', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['toggle']                     = array('Toggle Alert Email', 'Toggle record ID %s');
+
+$GLOBALS['TL_LANG']['tl_calculator_submission']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_calculator_submission']['published']                  = array('Published', 'Show this record on the front end.');
+
+$GLOBALS['TL_LANG']['tl_calculator_submission']['step_one_details']           = 'Step One - Details';
+$GLOBALS['TL_LANG']['tl_calculator_submission']['date_created']               = array('Date Created', 'The date this record was created');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['first_name']                 = array('First Name', 'The first name of the person who submitted this calculation');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['last_name']                  = array('Last Name', 'The last name of the person who submitted this calculation');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['company_name']               = array('Company Name', 'The company name filled in during Step One');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['project_name']               = array('Project Name', 'The project name filled in during Step One');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['zip_code']                   = array('Zip Code', 'The zip code filled in during Step One');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['phone_number']               = array('Phone Number', 'The phone number filled in during Step One');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['email_address']              = array('Email Address', 'The email address filled in during Step One');
+
+$GLOBALS['TL_LANG']['tl_calculator_submission']['step_two_details']           = 'Step Two - Details';
+$GLOBALS['TL_LANG']['tl_calculator_submission']['room_length']                = array('Room Length', 'The room length filled in during Step Two');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['room_width']                 = array('Room Width', 'The room width filled in during Step Two');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['room_height']                = array('Room Height', 'The room height filled in during Step Two');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['floor_material']             = array('Floor Material', 'The floor material selected during Step Two');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['ceiling_material']           = array('Ceiling Material', 'The ceiling material selected during Step Two');
+$GLOBALS['TL_LANG']['tl_calculator_submission']['wall_material']              = array('Wall Material', 'The wall material selected during Step Two');
