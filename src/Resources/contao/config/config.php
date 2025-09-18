230a233299dcbeefe0e2e890c355b6e360c894af
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-18 10:06:07 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    DDS Calculator
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/dds-calculator
+*/
+
+use Contao\System;
+
+/* Hooks */
+$GLOBALS['TL_HOOKS']['processFormData'][]        = array('Bcs\Hooks\FormHooks', 'onFormSubmit');
