9cdcfaa08674f7984786c12b74e5c534d8f6464c
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-18 12:37:15 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -9,35 +9,10 @@ class FormHooks
     // When a form is submitted
     public function onFormSubmit($submittedData, $formData, $files, $labels, $form)
     {
-        // BOOTSTRAP - For triggering one off scripts when the need arises
-        if($formData['formID'] == 'dds_step_1') {
-          //echo "HOOK: FORM HOOKED HOOKFULLY";
-          //die();
-        }
-        
-        else if ($formData['formID'] == 'dds_step_2') {
-            echo "<pre>";
-            print_r($submittedData);
-            die();
-        }
-        
-    }
-
-    public function onCompileFormFields($arrFields, $formId, $objForm)
-    {
-        if ($objForm->id == 'dds_step_2') {
-            // loop through all of the form fields
-            foreach ($arrFields as $field)
-            {
-                // if we have a value in post that matches this field's name
-                if (\Input::post($field->name) != '') 
-                    $field->value = \Input::post($field->name);
-            }	
-        }
-        
-        // return our modified form fields
-        return $arrFields;
+        echo "FORM SUBMITTED!<br>";
+        echo "<pre>";
+        print_r($submittedData);
+        die();
     }
     
-  
 }
