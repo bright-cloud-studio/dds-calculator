c1956ca68034585dba21079164de630cf042bc85
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-18 13:31:27 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -2,7 +2,7 @@
 
 namespace Bcs\Hooks;
 
-use Bcs\Model\CalculatorSubmission
+use Bcs\Model\CalculatorSubmission;
 
 class FormHooks
 {
@@ -11,24 +11,28 @@ class FormHooks
     // When a form is submitted
     public function onFormSubmit($submittedData, $formData, $files, $labels, $form)
     {
-        $calculator_record = new CalculatorRecord();
-        $calculator_record->tstamp = time();
-        $calculator_record->date_created = time();
+        $calculator_submission = new CalculatorSubmission();
+        $calculator_submission->tstamp = time();
+        $calculator_submission->date_created = time();
 
-        $calculator_record->first_name = $submittedData['first_name'];
-        $calculator_record->last_name = $submittedData['last_name'];
-        $calculator_record->company_name = $submittedData['company_name'];
-        $calculator_record->project_name = $submittedData['project_name'];
-        $calculator_record->zip_code = $submittedData['zip_code'];
-        $calculator_record->phone_number = $submittedData['phone_number'];
-        $calculator_record->email_address = $submittedData['email_address'];
+        $calculator_submission->first_name = $submittedData['first_name'];
+        $calculator_submission->last_name = $submittedData['last_name'];
+        $calculator_submission->company_name = $submittedData['company_name'];
+        $calculator_submission->project_name = $submittedData['project_name'];
+        $calculator_submission->zip_code = $submittedData['zip_code'];
+        $calculator_submission->phone_number = $submittedData['phone_number'];
+        $calculator_submission->email_address = $submittedData['email_address'];
 
-        $calculator_record->room_length = $submittedData['room_length'];
-        $calculator_record->room_width = $submittedData['room_width'];
-        $calculator_record->room_height = $submittedData['room_height'];
-        $calculator_record->floor_material = $submittedData['floor_material'];
-        $calculator_record->ceiling_material = $submittedData['ceiling_material'];
-        $calculator_record->wall_material = $submittedData['wall_material'];
+        $calculator_submission->room_length = $submittedData['room_length'];
+        $calculator_submission->room_width = $submittedData['room_width'];
+        $calculator_submission->room_height = $submittedData['room_height'];
+        $calculator_submission->floor_material = $submittedData['floor_material'];
+        $calculator_submission->ceiling_material = $submittedData['ceiling_material'];
+        $calculator_submission->wall_material = $submittedData['wall_material'];
+        
+        $calculator_submission->save();
+        
+        
         
     }
     
