a79761d494fa0af20eae480a5f19a8235d60142d
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-18 09:59:04 -0400

    Create BcsDDSCalcBundle.php

---
+++ src/BcsDDSCalcBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    DDS Calculator
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/dds-calculator
+ */
+
+namespace Bcs\DDSCalcBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsDDSCalcBundle extends Bundle
+{
+}
