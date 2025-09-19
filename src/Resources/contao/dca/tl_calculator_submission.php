6ed39758566191f0638ccf0f102047a25f2b4c1b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-19 13:29:05 -0400

    Update tl_calculator_submission.php

--- src/Resources/contao/dca/tl_calculator_submission.php
+++ src/Resources/contao/dca/tl_calculator_submission.php
@@ -19,10 +19,6 @@ $GLOBALS['TL_DCA']['tl_calculator_submission'] = array
         'notCopyable'                 => true,
         'notDeletable'                => true,
         'backendSearchIgnore'         => true,
-        'oncreate_callback' => array
-		(
-			array('tl_calculator_submission', 'generateUUID')
-		),
         'sql' => array
         (
             'keys' => array
@@ -294,21 +290,4 @@ class tl_calculator_submission extends Backend
         return $label;
     }
 
-    public function generateUUID($table, $insert_id, $record, $dc) {
-        // Generate 16 bytes (128 bits) of random data
-        $data = random_bytes(16);
-        
-        // Set version to 0100 (4) and variant to 10xx (RFC 4122)
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set variant to 10xx
-
-        $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-
-        Database::getInstance()
-            ->prepare("UPDATE tl_calculator_submission SET uuid=? WHERE id=?")
-            ->execute($uuid, $insert_id);
-        
-        return;
-    }
-    
 }
