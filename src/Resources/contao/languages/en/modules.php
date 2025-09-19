23c06ec1ed865decbb2a3d34bc9617f1ac15ae4e
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-19 12:49:22 -0400

    Update modules.php

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,5 +1,4 @@
 <?php
 
-
 /* Back End modules */
 $GLOBALS['TL_LANG']['MOD']['calculator_submission']               = array('Calculator Submissions', 'List records of submissions using the RT60 Calculator');
