8c973e0d62ef9029f2d304b8d1a7d23d41660f43
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-19 09:36:53 -0400

    Create tl_settings.php

---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['dds_calc_legend']          = 'RT60 Calculator - Settings';
+$GLOBALS['TL_LANG']['tl_settings']['zapier_webhook']           = array('Zapier Webhook', 'Enter the web address for the Zapier webhook.');
