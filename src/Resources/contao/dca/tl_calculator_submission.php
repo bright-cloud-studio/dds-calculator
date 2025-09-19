1f2c5c3aff5452f0efbb4ecfd5d771c60a6c8c6e
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-19 13:24:39 -0400

    Update tl_calculator_submission.php

--- src/Resources/contao/dca/tl_calculator_submission.php
+++ src/Resources/contao/dca/tl_calculator_submission.php
@@ -303,9 +303,12 @@ class tl_calculator_submission extends Backend
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set variant to 10xx
 
-        $dc->activeRecordd->uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-        $dc->activeRecord->save();
-        // Format the UUID string
+        $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+
+        Database::getInstance()
+            ->prepare("UPDATE tl_calculator_submission SET uuid=? WHERE id=?")
+            ->execute($uuid, $insert_id);
+        
         return;
     }
     
