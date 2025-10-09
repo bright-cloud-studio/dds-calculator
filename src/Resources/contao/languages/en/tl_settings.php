1398969d9afcd34f203a2a5771267151f69e46e8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-09 11:51:59 -0400

    Update tl_settings.php

--- src/Resources/contao/languages/en/tl_settings.php
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -2,3 +2,7 @@
 
 $GLOBALS['TL_LANG']['tl_settings']['dds_calc_legend']          = 'RT60 Calculator - Settings';
 $GLOBALS['TL_LANG']['tl_settings']['zapier_webhook']           = array('Zapier Webhook', 'Enter the web address for the Zapier webhook.');
+
+$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_1']           = array('Flag 1', 'Enter the copy to display when RT60 is greater than 1.5');
+$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_2']           = array('Flag 2', 'Enter the copy to display when RT60 is less than 0.6');
+$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_3']           = array('Flag 3', 'Enter the copy to display when RT60 is between 0.6 and 1.5');
