5b10fd80d5c647a10b658031dcf11bc6c11f32fa
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-09 11:49:28 -0400

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{dds_calc_legend},zapier_webhook;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{dds_calc_legend},zapier_webhook,rt60_flag_1,rt60_flag_2,rt60_flag_3;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
     'zapier_webhook' => [
@@ -8,5 +8,39 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
         'inputType'         => 'text',
         'default'           => '',
         'eval'              => ['tl_class' => 'w100'],
+    ],
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields'] += [
+    'rt60_flag_1' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_1'],
+        'inputType'         => 'textarea',
+        'default'           => '',
+        'search'            => false,
+        'filter'            => false,
+        'eval'              => array('mandatory'=>false, 'tl_class'=>'clr w100', 'rte'=>'tinyMCE', 'allowHtml' => true),
+        'sql'               => "text NOT NULL default ''"
+    ],
+];
+$GLOBALS['TL_DCA']['tl_settings']['fields'] += [
+    'rt60_flag_2' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_2'],
+        'inputType'         => 'textarea',
+        'default'           => '',
+        'search'            => false,
+        'filter'            => false,
+        'eval'              => array('mandatory'=>false, 'tl_class'=>'clr w100', 'rte'=>'tinyMCE', 'allowHtml' => true),
+        'sql'               => "text NOT NULL default ''"
+    ],
+];
+$GLOBALS['TL_DCA']['tl_settings']['fields'] += [
+    'rt60_flag_3' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['rt60_flag_3'],
+        'inputType'         => 'textarea',
+        'default'           => '',
+        'search'            => false,
+        'filter'            => false,
+        'eval'              => array('mandatory'=>false, 'tl_class'=>'clr w100', 'rte'=>'tinyMCE', 'allowHtml' => true),
+        'sql'               => "text NOT NULL default ''"
     ],
 ];
